b3b023bd104b051872d3f3c0f620202a21f167e8
hamayoonk38 <hamayoonk38@gmail.com>
2023-08-07 10:27:02 +0100

    constants defined - db_setup complete
    Variables for job_roles defined to use for searching roles on indeed, skills_list created to create auto-annotation for the description against, database connection established using sqlAlchemy (need to define the tables i.e. Jobs, Skills etc)

---
+++ constants/job_roles.py
@@ -0,0 +1,20 @@
+
+job_roles = [
+    {'role_title': 'Software Developer', 'query': 'Software (Developer OR Engineer)'},
+    {'role_title': 'Frontend Developer', 'query': '(Frontend OR Front-End) (Developer OR Engineer)'},
+    {'role_title': 'Backend Developer', 'query': '(Backend OR Back-End OR Server-side) (Developer OR Engineer)'},
+    {'role_title': 'Fullstack Developer', 'query': '(Full-Stack OR Fullstack) (Developer OR Engineer)'},
+    {'role_title': 'Mobile Developer', 'query': '(Mobile OR iOS OR Android) (Developer OR Engineer)'},
+    {'role_title': 'Game Developer', 'query': '(Game OR Gaming OR Video-Game) (Developer OR Engineer)'},
+    {'role_title': 'Data Scientist', 'query': 'Data (Scientist OR Analyst OR Engineer)'},
+    {'role_title': 'ML/AI Developer', 'query': '(Machine-Learning OR ML OR AI OR Artificial-Intelligence) (Engineer OR Specialist OR Developer)'},
+    {'role_title': 'DevOps Engineer', 'query': '(DevOps OR SRE) Engineer'},
+    {'role_title': 'System/Network Engineer', 'query': '(System OR Network) Engineer'},
+    {'role_title': 'Security Engineer', 'query': '(Cyber OR Information) Security (Engineer OR Analyst OR Specialist OR Consultant OR Manager)'},
+    {'role_title': 'DBA', 'query': 'Database Administrator OR DBA'},
+    {'role_title': 'Quality Assurance', 'query': '(QA OR Quality-Assurance) (Engineer OR Analyst OR Tester)'},
+    {'role_title': 'UX/UI Designer', 'query': '(UX OR UI OR User-Experience OR User-Interface) Designer'}, {'role_title': 'System Admin', 'query': 'System Administrator'},
+    {'role_title': 'Network Admin', 'query': 'Network Administrator'},
+    {'role_title': 'Cloud Architect', 'query': '(Cloud OR AWS OR Azure OR Google Cloud) Architect'},
+    {'role_title': 'Firmware/Embedded Developer', 'query': '(Firmware OR Embedded Systems) (Engineer OR Developer)'},
+    {'role_title': 'Solution/Enterprise Developer', 'query': '(Solution OR Enterprise) (Architect OR Developer OR Engineer)'}]
---
+++ database/db_setup.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import DRIVER_NAME, MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, DATABASE_NAME
+
+# Create SQLAlchemy engine to connect to MySQL
+mysql_engine = create_engine(
+    URL(
+        drivername=DRIVER_NAME,
+        username=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        host=MYSQL_HOST
+    )
+)
+
+# Create database if it doesn't exist
+with mysql_engine.connect() as connection:
+    connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME}"))
+    connection.execute(text(f"USE {DATABASE_NAME}"))
+
+url = URL.create(
+    drivername=DRIVER_NAME,
+    username=MYSQL_USER,
+    password=MYSQL_PASSWORD,
+    host=MYSQL_HOST,
+    database=DATABASE_NAME
+)
+
+db_engine = create_engine(url)
+
+# Create SQLAlchemy session bound to the database engine
+SessionLocal = sessionmaker(autoflush=False, autocomit=False, bind=db_engine)
+
+# Base class for declarative model definitions
+Base = declarative_base()
+
+Base.metadata.create_all(bind=db_engine)
